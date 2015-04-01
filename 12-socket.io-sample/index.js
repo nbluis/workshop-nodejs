f67455ed0e8e62ab14a8481410c7736cfece398e
Eduardo Bohrer <nbluisrs@gmail.com>
2015-04-01 20:57:16 -0300

    socket.io sample completo

--- 12-socket.io-sample/index.js
+++ 12-socket.io-sample/index.js
@@ -7,23 +7,36 @@ var users = 0;
 
 app.use(express.static(__dirname + '/public'));
 
+var nomes = [];
+
 io.on('connection', function (socket) {
 	users++
+	var nome = socket.handshake.query.nome;
+	nomes.push(nome);
 	socket.broadcast.emit('update-users', users);
 	socket.emit('update-users', users);
 
-	console.log('New client connected: Total ' + users);
+	socket.emit('nomes', nomes);
 
-	socket.on('send-message', function(data) {
-		console.log('Message received: '+ data);
-		socket.broadcast.emit('message-sent', data);
+	socket.on('enviar-mensagem', function(data) {
+		socket.broadcast.emit('recebeu-mensagem', data);
 	});
 
 	socket.on('disconnect', function() {
 		socket.broadcast.emit('update-users', --users);
+		for (var i = nomes.length - 1; i >= 0; i--) {
+			if(nomes[i]==nome){
+				delete nomes[i];
+				break;
+			}
+		};
+		socket.broadcast.emit('remover-nome', nome);
+
 	});
 });
 
-server.listen(3000, function() {
-	console.log('Server listening at: ' + 3000);
+var PORT = process.env.PORT || 3000;
+
+server.listen(PORT, function() {
+	console.log('Server listening at: ' + PORT);
 });
--- 12-socket.io-sample/public/main.js
+++ 12-socket.io-sample/public/main.js
@@ -1,25 +1,65 @@
 $(function() {
-	var socket = io.connect('http://localhost:3000');
+	$('.chat-section').hide();
 
-	function addMessage(message) {
-		$('.chat-panel').append($('<div></div>').text(message));
-	}
-
-	socket.on('message-sent', function(data) {
-		addMessage(data);
-	});
-
-	socket.on('update-users', function(users) {
-		console.log('users ' + users);
-		$('#chat-users').text(users);
-	});
-
-	$('#send-button').on('click', function() {
-		var messageBox = $('.chat-message');
-		if (messageBox.val().length > 0) {
-			addMessage(messageBox.val());
-			socket.emit('send-message', messageBox.val());
-			messageBox.val('');
+	$("#botao-login").on('click', function() {
+		var nome = $('#nome').val();
+		if (nome.length == 0) {
+			alert('Preenche o nome tchê!');
+		} else {
+			abreChat(nome);
 		}
 	});
+
+	function addMessage(data, eu) {
+		console.log(data);
+		var quem = (eu) ? 'eu' : '' ;
+		$('.chat-panel').append($('<div class="'+quem+'"></div>').text(data.usuario+': '+data.mensagem));
+	}
+
+	function abreChat(nome) {
+		console.log('Abri o chat');
+		$('.chat-section').show();
+		$('.identificacao').hide();
+
+		var socket = io.connect('http://localhost:3000', {query: 'nome=' + nome});
+
+		socket.on('recebeu-mensagem', function(data) {
+			addMessage(data, false);
+		});
+
+		socket.on('update-users', function(users) {
+			console.log('users ' + users);
+			$('#chat-users').text(users);
+			$('#chat-user').text(nome);
+		});
+
+		socket.on('nomes', function(nomes) {
+			for(var index in nomes) {
+				$('#nomes').append($('<li></li>').text(nomes[index]));
+			}
+		});
+
+		socket.on('remover-nome', function(nome) {
+			console.log('Removendo '+nome);
+			$('#nomes li').each(function(){
+				if($(this).text()==nome){
+					$(this).remove();
+				}
+			});
+		});
+
+		$('#send-button').on('click', function() {
+			var messageBox = $('.chat-message');
+			if (messageBox.val().length > 0) {
+				var mensagem = {
+					usuario: nome,
+					mensagem : messageBox.val()
+				}
+				addMessage(mensagem, true);
+				socket.emit('enviar-mensagem', mensagem);
+				messageBox.val('');
+			}
+		});
+	}
+
 });
